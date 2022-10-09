ee0748e690d656312322ba265c55c1cd739545ff
TheClassicTechno <92133615+TheClassicTechno@users.noreply.github.com>
2022-10-09 18:14:58 -0400

    Update shoe_authenticity_detector_sire.py

--- shoe_authenticity_detector_sire.py
+++ shoe_authenticity_detector_sire.py
@@ -9,9 +9,44 @@ Original file is located at
 
 from google.colab import drive
 drive.mount('/content/drive')
-
-#processing images
 path = '/content/drive/MyDrive/shoedataset'
+import requests
+
+def download_file_from_google_drive(id, destination):
+    URL = path
+
+    session = requests.Session()
+
+    response = session.get(URL, params = { 'id' : id }, stream = True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = { 'id' : id, 'confirm' : token }
+        response = session.get(URL, params = params, stream = True)
+
+    save_response_content(response, destination)    
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+
+if __name__ == "__main__":
+    file_id = 'TAKE ID FROM SHAREABLE LINK'
+    destination = 'DESTINATION FILE ON YOUR DISK'
+    download_file_from_google_drive(file_id, destination)
+#processing images
+
 
 """created by Julia Huang"""
 

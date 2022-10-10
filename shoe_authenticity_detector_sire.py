d044fddca412641ad09db5faf6d16614568ac525
TheClassicTechno <92133615+TheClassicTechno@users.noreply.github.com>
2022-10-10 16:39:22 -0400

    Update shoe_authenticity_detector_sire.py

--- shoe_authenticity_detector_sire.py
+++ shoe_authenticity_detector_sire.py
@@ -16,9 +16,44 @@ from st_btn_select import st_btn_select
 
 selection = st_btn_select(('CHECK YOUR SHOES', 'ABOUT'))
 
-st.markdown(""" <style> .font {
-    background-color: #e7e3ff;} 
-    </style> """, unsafe_allow_html=True)
+st.markdown(
+    
+<style>
+.reportview-container .markdown-text-container {
+    font-family: monospace;
+}
+.sidebar .sidebar-content {
+    background-image: linear-gradient(#2e7bcf,#2e7bcf);
+    color: white;
+}
+.Widget>label {
+    color: white;
+    font-family: monospace;
+}
+[class^="st-b"]  {
+    color: white;
+    font-family: monospace;
+}
+.st-bb {
+    background-color: transparent;
+}
+.st-at {
+    background-color: #0c0080;
+}
+footer {
+    font-family: monospace;
+}
+.reportview-container .main footer, .reportview-container .main footer a {
+    color: #0c0080;
+}
+header .decoration {
+    background-image: none;
+}
+
+</style>
+
+    unsafe_allow_html=True,
+)
 if selection == 'CHECK YOUR SHOES':
     
     st.markdown(""" <style> .font {

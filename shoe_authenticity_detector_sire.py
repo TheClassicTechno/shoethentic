30dc7cac42f704beff38d45f8650874bb346e8bb
TheClassicTechno <92133615+TheClassicTechno@users.noreply.github.com>
2022-10-10 17:34:11 -0400

    Update shoe_authenticity_detector_sire.py

--- shoe_authenticity_detector_sire.py
+++ shoe_authenticity_detector_sire.py
@@ -111,57 +111,47 @@ if selection == 'ABOUT':
     st.markdown('<p class="font">About Shoethentic</p>', unsafe_allow_html=True)
    
 
-    st.markdown(""" <style> .font {
+    st.markdown(""" <style> .font2 {
     font-size:40px ; font-weight: 800; color: #fffafa;} 
     </style> """, unsafe_allow_html=True)
-    st.markdown('<p class="font">About the Creator</p>', unsafe_allow_html=True)
+    st.markdown('<p class="font2">About the Creator</p>', unsafe_allow_html=True)
 
-    st.markdown(""" <style> .font {
+    st.markdown(""" <style> .font3 {
     font-size:25px ; font-weight: 600; color: #fffafa;} 
     </style> """, unsafe_allow_html=True)
-    st.markdown('<p class="font">The web app and model portion is built by Julia Huang, a current student and developer at Sire, and the dataset is created by Justin Huang.</p>', unsafe_allow_html=True)
+    st.markdown('<p class="font3">The web app and model portion is built by Julia Huang, a current student and developer at Sire, and the dataset is created by Justin Huang.</p>', unsafe_allow_html=True)
   
     
     
-    st.markdown(""" <style> .font {
+    st.markdown(""" <style> .font2 {
     font-size:40px ; font-weight: 800; color: #fffafa;} 
     </style> """, unsafe_allow_html=True)
-    st.markdown('<p class="font">Mission</p>', unsafe_allow_html=True)
+    st.markdown('<p class="font2">Mission</p>', unsafe_allow_html=True)
 
-    st.markdown(""" <style> .font {
+    st.markdown(""" <style> .font3 {
     font-size:25px ; font-weight: 600; color: #fffafa;} 
     </style> """, unsafe_allow_html=True)
-    st.markdown('<p class="font">Due to the high prevalence of counterfeit shoe production, the goal of **Shoethentic** is to provide the sneakerhead community an opportunity to check the authenticity of each and every shoe they buy. **Shoethentic** aims to make this checking process simpler and more convenient by utilizing AI & machine learning.</p>', unsafe_allow_html=True)
+    st.markdown('<p class="font3">Due to the high prevalence of counterfeit shoe production, the goal of **Shoethentic** is to provide the sneakerhead community an opportunity to check the authenticity of each and every shoe they buy. **Shoethentic** aims to make this checking process simpler and more convenient by utilizing AI & machine learning.</p>', unsafe_allow_html=True)
     
-    st.markdown(""" <style> .font {
+    st.markdown(""" <style> .font2 {
     font-size:40px ; font-weight: 800; color: #fffafa;} 
     </style> """, unsafe_allow_html=True)
-    st.markdown('<p class="font">How Shoethentic was Built"</p>', unsafe_allow_html=True)
+    st.markdown('<p class="font2">How Shoethentic was Built"</p>', unsafe_allow_html=True)
 
-    st.markdown(""" <style> .font {
+    st.markdown(""" <style> .font3 {
     font-size:25px ; font-weight: 600; color: #fffafa;} 
     </style> """, unsafe_allow_html=True)
-    st.markdown('<p class="font">Shoethentic has two parts: the AI model and web app. The AI model is built using the TensorFlow framework in the Python Language while the web app is built using Streamlit. We trained the model in Google Colab on a dataset consisting of fake and real shoe images sourced from the CheckCheck mobile app and deployed it into the web app.</p>', unsafe_allow_html=True)
+    st.markdown('<p class="font3">Shoethentic has two parts: the AI model and web app. The AI model is built using the TensorFlow framework in the Python Language while the web app is built using Streamlit. We trained the model in Google Colab on a dataset consisting of fake and real shoe images sourced from the CheckCheck mobile app and deployed it into the web app.</p>', unsafe_allow_html=True)
     
-    st.markdown(""" <style> .font {
+    st.markdown(""" <style> .font2 {
     font-size:40px ; font-weight: 800; color: #fffafa;} 
     </style> """, unsafe_allow_html=True)
-    st.markdown('<p class="font">Future of Shoethentic</p>', unsafe_allow_html=True)
+    st.markdown('<p class="font2">Future of Shoethentic</p>', unsafe_allow_html=True)
 
-    st.markdown(""" <style> .font {
+    st.markdown(""" <style> .font3 {
     font-size:25px ; font-weight: 600; color: #fffafa;} 
     </style> """, unsafe_allow_html=True)
-    st.markdown('<p class="font">We plan to improve the accuracy of the AI model even more when checking for shoes and integrate it into the Sire website later on.</p>', unsafe_allow_html=True)
+    st.markdown('<p class="font3">We plan to improve the accuracy of the AI model even more when checking for shoes and integrate it into the Sire website later on.</p>', unsafe_allow_html=True)
     
     
-    '''
-    st.subheader("Mission")
-    st.markdown("Due to the high prevalence of counterfeit shoe production, the goal of **Shoethentic** is to provide the sneakerhead community an opportunity to check the authenticity of each and every shoe they buy. **Shoethentic** aims to make this checking process simpler and more convenient by utilizing AI & machine learning.")
-    st.subheader("How Shoethentic was Built")
-  
-    st.markdown("Shoethentic has two parts: the AI model and web app. The AI model is built using the TensorFlow framework in the Python Language while the web app is built using Streamlit. We trained the model in Google Colab on a dataset consisting of fake and real shoe images sourced from the CheckCheck mobile app and deployed it into the web app.")
- 
-    st.subheader("Future of Shoethentic")
-    st.markdown("We plan to improve the accuracy of the AI model even more when checking for shoes and integrate it into the Sire website later on.")
-    '''
-
+    
